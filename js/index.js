e688b5ae419c78c964b4d4a312fd63fb96f7c0f6
杨杰 <616651001@qq.com>
2018-01-31 15:21:11 +0800

    添加了触摸事件的代码

--- js/index.js
+++ js/index.js
@@ -25,13 +25,20 @@ function bannerTouch() {
 	// 找到 ul 元素
 	var oUl = oBanner.querySelector(".pic")
 	
-	var startX = 0;		// 开始点的x坐标
+	var touchStartX = 0;	// 触摸开始的点
+	var startX = 0;			// 开始点的x坐标
+	var currentX = 0;		// 获取轮播图当前X值
 	
 	// 1. 给banner添加触摸事件
 	oBanner.addEventListener("touchstart", function(e) {
 		
+		currentX = oUl.offsetLeft;		// 在触摸开始时保存ul的起始位置
+		
 		// 得到ul的偏移值
 		var offsetX = oUl.offsetLeft;
+		
+		
+		touchStartX = e.touches[0].clientX; 	// 触摸开始的内容
 		
 		startX = e.touches[0].clientX;	// 起始点x坐标值
 		startX -= offsetX;				// 加上初始ul的偏移值
@@ -39,16 +46,43 @@ function bannerTouch() {
 		 console.log("触摸开始", startX)
 	})
 	
+	var endX = 0;				// 定义全局变量保存触摸点的x值
+	
 	oBanner.addEventListener("touchmove", function(e) {
-		var x = e.touches[0].clientX;  // 移动的x坐标值
+		endX = e.touches[0].clientX;  	// 移动的x坐标值
 		
-		var detaX = x - startX; 		// 与起始点的x差值
+		var detaX = endX - startX; 		// 与起始点的x差值
 		
-		 console.log("触摸移动", startX, x, detaX)
+//		 console.log("触摸移动", startX, endX, detaX)
 		
 		// 通过差值，修改 ul 的位置
 		oUl.style.marginLeft = detaX+"px"
 		
+	})
+	
+	// 【存在问题】 如果只触摸一下，那么并没有移动，怎么办？
+	// 思路：
+	//   1. 找到松手的位置 endX
+	//   2. 与 touchStartX 之间进行比较
+	//             决定是往左边还是往右滑
+	//   3. 设置轮播图 移动对应 一张图片的 距离
+	oBanner.addEventListener("touchend", function(e) {
+
+		var oLi = oUl.querySelector("li");  	// 得到li
+	
+//		console.log("touchStartX: " + touchStartX, "endX: " + endX)
+		if (touchStartX > endX) {  	// 往左滑
+			// 当前x  减去  1张图片的宽度
+			var x = currentX - oLi.offsetWidth
+			
+		} else {				// 往右滑
+			var x = currentX + oLi.offsetWidth
+		}
+		
+		console.log("end:", x)
+		
+		// 更新ul的left值
+		oUl.style.marginLeft = x + "px"
 	})
 	
 	
